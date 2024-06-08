1dcf21af3f9c59a446b68cb5db89643e4c6a38d3
Alon Brand <alonbrand01@gmail.com>
2024-06-08 19:09:07 +0300

    Fix Procfile and configuration issues

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from flask_cors import CORS
 from utils.utils import *
@@ -373,5 +374,6 @@ def clean_users():
     except Exception as e:       
         print(e)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
