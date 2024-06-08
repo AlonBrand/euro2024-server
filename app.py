b39272b36e46fa37317398aa202183fe5e95f54d
Alon Brand <alonbrand01@gmail.com>
2024-06-08 18:56:16 +0300

    .

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # main.py
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,6 @@ app = Flask(__name__)
 def home():
     return "Hello, Heroku!"
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
